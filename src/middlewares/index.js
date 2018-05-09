b4dc56e7aa6afc6c97359e158adab5a7fd2eedfc
FrankYang0529 <yangpoan@gmail.com>
2018-05-09 16:25:20 +0800

    add middlewares me

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,10 +1,12 @@
 import { all } from 'redux-saga/effects'
 
 import watchLogin from './login'
+import watchMe from './me'
 
 function *rootSaga() {
   yield all([
-    watchLogin()
+    watchLogin(),
+    watchMe()
   ])
 }
 
