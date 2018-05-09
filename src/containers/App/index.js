20c7f1ee876d6ff377dce12412147b36747726be
FrankYang0529 <yangpoan@gmail.com>
2018-05-09 16:03:43 +0800

    add redux for App & LoginPage

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -5,39 +5,41 @@ import {
   Route,
   Redirect
 } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 import HomePage from '../HomePage'
 import LoginPage from '../LoginPage'
 import BookListPage from '../BookListPage'
 import NewBookPage from '../NewBookPage'
 
-export default class App extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      loggedIn: false
-    }
-  }
+class App extends React.Component {
   render() {
-    const { loggedIn } = this.state
+    const { user } = this.props
 
     return (
       <Router>
         <Switch>
           <Route
             exact path='/'
-            render={props => loggedIn ? <BookListPage /> : <HomePage />}
+            render={props => user ? <BookListPage /> : <HomePage />}
           />
           <Route
             path='/login'
-            render={props => loggedIn ? <Redirect to={{pathname: '/'}} /> : <LoginPage />}
+            render={props => user ? <Redirect to={{pathname: '/'}} /> : <LoginPage />}
           />
           <Route
             path='/new'
-            render={props => loggedIn ? <NewBookPage /> : <Redirect to={{pathname: '/login'}} />}
+            render={props => user ? <NewBookPage /> : <Redirect to={{pathname: '/login'}} />}
           />
         </Switch>
       </Router>
     )
   }
 }
+
+const mapStateToProps = store => {
+  const { user } = store.auth
+  return { user }
+}
+
+export default connect(mapStateToProps)(App)
--- src/containers/LoginPage/index.js
+++ src/containers/LoginPage/index.js
@@ -1,7 +1,45 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-export default class LoginPage extends React.Component {
-  render() {
-    return 'Login Page'
+import { onLoginRequest } from '../../actions/auth'
+
+export const LoginPage = ({ onLoginRequestAction }) => {
+  let emailField = null
+  let passwordField = null
+
+  return (
+    <form>
+      <input
+        type='text'
+        ref={el => emailField = el}
+      />
+      <input
+        type='password'
+        ref={el => passwordField = el}
+      />
+      <button
+        type='submit'
+        onClick={(e) => {
+          e.preventDefault()
+          onLoginRequestAction({
+            email: emailField.value,
+            password: passwordField.value
+          })
+
+          passwordField.value = ''
+        }}
+      > Login
+      </button>
+    </form>
+  )
+}
+
+export function mapDispatchToProps(dispatch) {
+  return {
+    onLoginRequestAction: (username, password) => {
+      dispatch(onLoginRequest(username, password))
+    }
   }
 }
+
+export default connect(null, mapDispatchToProps)(LoginPage)
