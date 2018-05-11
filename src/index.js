b6bdcb45f591feb80dd8b1e073aad195869754ff
FrankYang0529 <yangpoan@gmail.com>
2018-05-11 11:41:57 +0800

    fix format

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import createSagaMiddleware from 'redux-saga'
@@ -7,7 +8,7 @@ import axios from 'axios'
 
 import * as reducers from './reducers'
 import rootSaga from './middlewares'
-import App from './containers/App';
+import App from './containers/App'
 
 axios.defaults.baseURL = process.env.REACT_APP_API_HOST
 
--- src/middlewares/login.js
+++ src/middlewares/login.js
@@ -2,23 +2,23 @@ import { call, put, take } from 'redux-saga/effects'
 
 import { onLoginSuccess } from '../actions/auth'
 import { LOGIN_REQUEST } from '../constants/auth'
-import { login as loginApi} from '../api'
+import { login as loginApi } from '../api'
 
-export function* login(action) {
+export function * login (action) {
   try {
     const { user, message } = yield call(loginApi, action.payload)
     if (!user) {
       throw new Error(message)
     }
 
-    localStorage.setItem('user', JSON.stringify(user))
+    window.localStorage.setItem('user', JSON.stringify(user))
     yield put(onLoginSuccess({ user }))
   } catch (error) {
     console.log(error)
   }
 }
 
-function* watchLogin() {
+function * watchLogin () {
   while (1) {
     const action = yield take(LOGIN_REQUEST)
     yield call(login, action)
