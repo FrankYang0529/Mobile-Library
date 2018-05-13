684eab85db3c1f420c47a066c9e1a40dd403c392
FrankYang0529 <yangpoan@gmail.com>
2018-05-13 12:12:40 +0800

    Implement LoginPage

--- src/containers/LoginPage/index.js
+++ src/containers/LoginPage/index.js
@@ -1,43 +1,91 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  Col,
+  Form,
+  FormGroup,
+  Label,
+  Input
+} from 'reactstrap'
 
 import { onLoginRequest } from '../../actions/auth'
 
-export const LoginPage = ({ onLoginRequestAction }) => {
-  let emailField = null
-  let passwordField = null
+class LoginPage extends React.Component {
+  constructor (props) {
+    super(props)
 
-  return (
-    <form>
-      <input
-        type='text'
-        ref={el => emailField = el}
-      />
-      <input
-        type='password'
-        ref={el => passwordField = el}
-      />
-      <button
-        type='submit'
-        onClick={(e) => {
-          e.preventDefault()
-          onLoginRequestAction({
-            email: emailField.value,
-            password: passwordField.value
-          })
+    this.emailRef = React.createRef()
+    this.passwordRef = React.createRef()
+  }
 
-          passwordField.value = ''
-        }}
-      > Login
-      </button>
-    </form>
-  )
+  render () {
+    const { onLoginRequestAction } = this.props
+
+    return (
+      <div className='row'>
+        <div className='col-md-6 offset-md-3'>
+          <Card>
+            <CardHeader className='text-center'>Login</CardHeader>
+            <CardBody>
+              <Form>
+                <FormGroup row>
+                  <Label for='email' sm={3}>Email</Label>
+                  <Col sm={9}>
+                    <Input
+                      type='email'
+                      name='email'
+                      id='email'
+                      placeholder='Please input your email.'
+                      innerRef={this.emailRef}
+                    />
+                  </Col>
+                </FormGroup>
+                <FormGroup row>
+                  <Label for='password' sm={3}>Password</Label>
+                  <Col sm={9}>
+                    <Input
+                      type='password'
+                      name='password'
+                      id='password'
+                      placeholder='Please input your password.'
+                      innerRef={this.passwordRef}
+                    />
+                  </Col>
+                </FormGroup>
+                <Button
+                  block
+                  color='success'
+                  type='submit'
+                  onClick={(e) => {
+                    e.preventDefault()
+                    console.log(this.emailRef.current.value)
+                    console.log(this.passwordRef.current.value)
+                    onLoginRequestAction({
+                      email: this.emailRef.current.value,
+                      password: this.passwordRef.current.value
+                    })
+
+                    this.passwordRef.current.value = ''
+                  }}
+                > Login
+                </Button>
+              </Form>
+            </CardBody>
+          </Card>
+        </div>
+      </div>
+    )
+  }
 }
 
-export function mapDispatchToProps(dispatch) {
+export function mapDispatchToProps (dispatch) {
   return {
-    onLoginRequestAction: (username, password) => {
-      dispatch(onLoginRequest(username, password))
+    onLoginRequestAction: ({ email, password }) => {
+      dispatch(onLoginRequest({ email, password }))
     }
   }
 }
